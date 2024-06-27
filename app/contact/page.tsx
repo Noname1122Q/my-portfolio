ccf1a0f6e2407eb8dd52c6195ee78cb7a31afc77
Noname1122Q <124861867+Noname1122Q@users.noreply.github.com>
2024-06-27 19:51:56 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -67,7 +67,6 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           <div className="xl:w-[54%] order-2 xl:order-none">
-              <h3 className="text-4xl text-blue-600">Let's work together</h3>
             <form onSubmit={sendEmail} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-blue-600">Let&#39;s work together</h3>
               <p className="text-white/60">
